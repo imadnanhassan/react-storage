13520c5b51c30c6f2388eefa91d6b4ecce48f01b
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2022-12-14 18:46:38 +0600

    react stroage and hendler

---
+++ src/components/Address/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Address = () => {
+   return (
+      <div>
+         <p>Address: Bangadesh</p>
+      </div>
+   );
+};
+
+export default Address;
---
+++ src/components/Myself/Myself.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Myself = () => {
+   return (
+      <div>
+         <h2>Im Adnan Hassan</h2>
+      </div>
+   );
+};
+
+export default Myself;
